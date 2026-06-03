5588b247b37f9254c9bd203321c7368eb08d3ef1
ebolyu13 <ebolyu13@users.noreply.github.com>
2026-06-04 00:07:28 +0800

    Add H8 element and validation cases

---
+++ STAPpp-master-2bfc1f7f02cb98acb452ebd48627272253747101/src/h/Material.h
@@ -0,0 +1,98 @@
+/*****************************************************************************/
+/*  STAP++ : A C++ FEM code sharing the same input data file with STAP90     */
+/*     Computational Dynamics Laboratory                                     */
+/*     School of Aerospace Engineering, Tsinghua University                  */
+/*                                                                           */
+/*     Release 1.11, November 22, 2017                                       */
+/*                                                                           */
+/*     http://www.comdyn.cn/                                                 */
+/*****************************************************************************/
+
+#pragma once
+
+#include "Outputter.h"
+
+using namespace std;
+
+//!	Material base class which only define one data member
+/*!	All type of material classes should be derived from this base class */
+class CMaterial
+{
+public:
+
+	unsigned int nset;	//!< Number of set
+	
+	double E;  //!< Young's modulus
+
+public:
+
+//! Virtual deconstructor
+    virtual ~CMaterial() {};
+
+//!	Read material data from stream Input
+	virtual bool Read(ifstream& Input) = 0;
+
+//!	Write material data to Stream
+    virtual void Write(COutputter& output) = 0;
+
+};
+
+//!	Material class for bar element
+class CBarMaterial : public CMaterial
+{
+public:
+
+	double Area;	//!< Sectional area of a bar element
+
+public:
+	
+//!	Read material data from stream Input
+	virtual bool Read(ifstream& Input);
+
+//!	Write material data to Stream
+	virtual void Write(COutputter& output);
+};
+
+//! Material class for beam element
+class CBeamMaterial : public CMaterial
+{
+public:
+
+	double Area;	//!< Cross-sectional area
+	double I;		//!< Moment of inertia
+
+public:
+
+	virtual bool Read(ifstream& Input);
+	virtual void Write(COutputter& output);
+};
+
+//! Material class for H8 3D solid element
+class CH8Material : public CMaterial
+{
+public:
+
+	double Nu;		//!< Poisson's ratio
+
+public:
+
+	virtual bool Read(ifstream& Input);
+	virtual void Write(COutputter& output);
+};
+
+//! Material class for Q4 element
+class CQ4Material : public CMaterial
+{
+public:
+
+	double Nu;		//!< Poisson's ratio
+	double Thickness;	//!< Element thickness
+
+public:
+
+//! Read material data from stream Input
+	virtual bool Read(ifstream& Input);
+
+//! Write material data to Stream
+	virtual void Write(COutputter& output);
+};
